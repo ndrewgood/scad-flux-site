cb2ea9bfa2adfdba606d223c60b1e7f41ffcd263
Eric Flatt <36873954+ericflatt@users.noreply.github.com>
2020-09-13 01:26:21 -0400

    form submission working

--- web/src/components/newsletter.js
+++ web/src/components/newsletter.js
@@ -8,9 +8,9 @@ const newsletter = (props) => {
             <div className="container">
                 <h1 className="title medium center">Want to know more?</h1>
                 <p className="body center">Sign up for our weekly newsletter for the latest FLUX updates</p>
-                <form className="inputContainer">
+                <form action="https://send.pageclip.co/0UiYhDA0j1IX44dBk33MbopV8pazPw22" class="pageclip-form" method="post" className="inputContainer">
                     <input type="email" placeholder="SCAD Email Address" name="scademail" id="newsletter-input" className="outlined"/>
-                    <button className="blueButton" type="submit">Subscribe</button>
+                    <button className="blueButton pageclip-form__submit" type="submit">Subscribe</button>
                 </form>
             </div>
         </section>            
