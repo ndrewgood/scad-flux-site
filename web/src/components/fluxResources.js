ff77cf1e22131593b3c212988661cdb690437e1a
Andrew G <gandrew888@gmail.com>
2020-09-29 12:45:25 -0400

    commented out mentorship block

--- web/src/components/fluxResources.js
+++ web/src/components/fluxResources.js
@@ -11,7 +11,7 @@ const fluxResources = () => {
                 <h1 className="title medium">FLUX Resources</h1>
                 <div className="threeGrid">
                     <ResourceCard title="Join the FLUX Discord!" description="We have a Discord! Join us for the latest FLUX updates, share cool ideas, meet new people, and have fun!" cta="Join the Discord here" link="https://discord.gg/FMJ4tY8" image={discord}/>
-                    <ResourceCard title="Mentor / Mentee Program" description="Our mentorship program is still happening this quarter! Stay tuned in our Discord or Instagram for the latest updates!" cta="More info coming soon!" link="" image={mentor}/>
+                    {/* <ResourceCard title="Mentor / Mentee Program" description="Our mentorship program is still happening this quarter! Stay tuned in our Discord or Instagram for the latest updates!" cta="More info coming soon!" link="" image={mentor}/> */}
                     <ResourceCard title="FLUX Medium Publication" description="FLUX will be publishing medium articles to help support you through these turbulent times! Want to write or submit an article? Let us know!" cta="View FLUX Medium" link="https://medium.com/scadflux" image={medium}/>
                 </div>
             </div>
