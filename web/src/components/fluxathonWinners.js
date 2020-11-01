127ea4bfb5e15c9f034a0e9ff51226714a07dc24
Varun Khatri <VarunMK05@gmail.com>
2020-11-01 13:32:50 -0500

    fluxathon winners completed
    added content and all three winners.

--- web/src/components/fluxathonWinners.js
+++ web/src/components/fluxathonWinners.js
@@ -3,8 +3,10 @@ import WinnerCard from './winnerCard'
 
 import '../styles/fluxathonWinners.scss'
 import '../styles/winnerCard.scss'
-import Photo from '../assets/startup.jpg'
-import winnerCard from './winnerCard'
+
+import FirstPhoto from '../assets/fluxathonFirst.jpg'
+import SecondPhoto from '../assets/fluxathonSecond.jpg'
+import ThirdPhoto from '../assets/fluxathonThird.jpg'
 
 const fluxathonWinners = (props) => {
     return(
@@ -14,14 +16,40 @@ const fluxathonWinners = (props) => {
                     <h1 className="title medium">FLUXathon x Duolingo Winners</h1>
                     <div className="communityDescription">
                         <p className="body medium">FLUXathon Fall 2020 was our BIGGEST Fluxathon yet with over 20 team registered to complete. On top of that, it was the first time we had a company sponsor, Duolingo. We had a great time collaborating with them and the projects that came out of this 24-hour design sprint were amazing.</p>
-                        <a target="_blank" href="https://scad.zoom.us/rec/play/CeoURNlzdS4H-JUWZilAVdc2rPWItzNowmrw2TLvnOfOQ2oLYlt1Q105oWphLaKez5ZuobYg3zOKMxMr.lar9H3VAhkfBUlXf?startTime=1603487218000&_x_zm_rtaid=dhLUsymvTAS5K81-HJZpVA.1603989174671.8fa3e31b9371a6ea35095d05c22c3e52&_x_zm_rhtaid=927"><button className="whiteButton">Watch Presentations</button></a>
+                        <a target="_blank" href="https://docs.google.com/presentation/d/1SaiEO5hwfPRk635E35VNDGKuxSu_AYT86HYStXe9WBA/edit?usp=sharing"><button className="whiteButton">View Kickoff Deck</button></a>
                     </div>
                 </div>
-                <WinnerCard/>
-                <ul className="slider-buttons">
-                    <li>«</li>
-                    <li>»</li>
-                </ul>
+                <WinnerCard 
+                    ranking="1st Place" 
+                    rankColor="gold" 
+                    teamName="Reefine (Team Sea Simps)" 
+                    description="Reefine, a credit card where users can spend money, directly donate to ocean conservation efforts—and even collect rewards. Whenever users spend money, they have the option to round up and donate to a cause of their choice—from ocean cleanups, to coral restorations, and more. Users can also earn cashback and redeemable rewards from the contributions they make." 
+                    team1="Mikayla Kim" team1Detail="UX Senior" 
+                    team2="Sunny Lee" team2Detail="Graphic Design Senior" 
+                    team3="Katsy Garcia" team3Detail="Illustration Senior" 
+                    url="https://docs.google.com/presentation/d/16dm6fC71Am0XVUjLIwroguYpy0wEQgj3CAo-sUjyMGI/edit?usp=sharing" 
+                    photo={FirstPhoto}/>
+                <WinnerCard 
+                    ranking="2nd Place" 
+                    rankColor="silver" 
+                    teamName="Anew (Team Momento Mori)" 
+                    description="Anew is a conversational application that aids in fostering dialogue between parents and their children to help them better understand the concept of death. The game uses storytelling and interactive elements to subtlely bring up the conversation of death to the child in a friendly understandable way. Activities include celebrating a dead grandmother's birth anniversary by baking a cake." 
+                    team1="Aparna Somvanshi" team1Detail="UX Junior" 
+                    team2="Nandika Gupta" team2Detail="UX Senior" 
+                    team3="Varun Khatri" team3Detail="UX Junior" 
+                    team4="James Fookes" team4Detail="Illustration Senior" 
+                    url="https://docs.google.com/presentation/d/1VtjR5TEcNE-ueGi7wOiG2Ud7CYYr3FaWu3zGcz921pc/edit?usp=sharing" 
+                    photo={SecondPhoto}/>
+                <WinnerCard 
+                    ranking="3rd Place" 
+                    rankColor="bronze" 
+                    teamName="Terratory" 
+                    description="TerraTory is an online, multiplayer game that encourages its users to recycle. By using popular open-world RPG gaming styles, TerraTory challenges both the user’s mental model of what a game is and also of recycling. The previously arduous task now takes on a new, fresh, and fun meaning. Plus, playing with your friends not only encourages friendly competition, but a better planet overall."
+                    team1="Arjun Gupta" team1Detail="UX Junior"
+                    team2="Ovadia Cohen" team2Detail="Industrial Design Junior"
+                    team3="Quint Bailey" team3Detail="UX Junior"
+                    url="https://docs.google.com/presentation/d/1kKIAsWSon1k7wDU7iVgRyGzqzxDSosbV9o3Zkh7Cn9I/edit?usp=sharing" 
+                    photo={ThirdPhoto}/>
             </div>
         </section>
     )
--- web/src/components/winnerCard.js
+++ web/src/components/winnerCard.js
@@ -1,27 +1,40 @@
 import React from 'react'
 import '../styles/winnerCard.scss'
-import Photo from '../assets/startup.jpg'
 
 const winnerCard = (props) => {
+    var rankingClass = "body regular semi-bold " + props.rankColor;
+
     return(
-        <div className="winnerCard">
-            <img src={Photo}></img>
+        <a className="winnerCard" href={props.url} target="_blank">
+            <img src={props.photo}></img>
             <div>
                 <div className="projectTitle">
-                    <h2 className="body large bold">Team Name</h2>
-                    <h3 className="body regular semi-bold gold">1st Place</h3>
+                    <h2 className="body large bold">{props.teamName}</h2>
+                    <h3 className={rankingClass}>{props.ranking}</h3>
                 </div>
-                <p class="projectDescription">Here is a description of the project. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed accumsan pellentesque felis ac volutpat. Vestibulum ut nulla scelerisque, luctus nibh eu, luctus turpis. Vestibulum vitae viverra libero. Etiam ac mi dapibus, hendrerit nisl ac, consequat nunc. Donec molestie ornare pellentesque.</p>
-                <h3 className="body regular bold">Team</h3>
+                <p class="projectDescription">{props.description}</p>
+                <h3 className="body regular bold">Team Members</h3>
                 <div className="teamGrid">
-                    <p>Andrew Goodridge - UX Junior</p>
-                    <p>Varun Khatri - UX Junior</p>
-                    <p>Eric Flatt - UX Senior</p>
-                    <p>Nico Zafarana - UX Junior</p>
+                    <div>
+                        <p className="memberName">{props.team1}</p>
+                        <p className="body small">{props.team1Detail}</p>
+                    </div>
+                    <div>
+                        <p className="memberName">{props.team2}</p>
+                        <p className="body small">{props.team2Detail}</p>
+                    </div>
+                    <div>
+                        <p className="memberName">{props.team3}</p>
+                        <p className="body small">{props.team3Detail}</p>
+                    </div>
+                    <div>
+                        <p className="memberName">{props.team4}</p>
+                        <p className="body small">{props.team4Detail}</p>
+                    </div>
                 </div>
                 <p className="viewMore body small semi-bold">Learn More</p>
             </div>
-        </div>
+        </a>
     )
 }
 
